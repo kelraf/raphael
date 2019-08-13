1e9b48df1d0d6db0a5c451485ed66f822919406c
= <=>
2019-08-13 07:38:28 +0300

    added bunner image and updated nav icons

--- js/custom/index.js
+++ js/custom/index.js
@@ -1,3 +1,24 @@
 // $(document).ready(function() {
 //     alert("Success");
 // });
+
+// Navigation
+
+let nav_side = document.getElementById("nav-cont-side");
+let docker = document.getElementById("docker");
+let line_one = document.getElementById("line-one");
+let line_two = document.getElementById("line-two");
+let line_three = document.getElementById("line-three");
+
+if(nav_side && docker && line_two && line_one && line_three) {
+    docker.onclick = function() {
+        if (line_one.style.top == "2px") {
+            alert("Success");
+        } else {
+            console.log(line_one.style);
+        }
+    }
+}
+
+// End of Navigation
+
