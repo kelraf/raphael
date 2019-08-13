112abd1fe5774ee7219269941ccf3f100ed49ee8
= <=>
2019-08-13 10:09:27 +0300

    edited nav

--- js/custom/index.js
+++ js/custom/index.js
@@ -4,18 +4,37 @@
 
 // Navigation
 
-let nav_side = document.getElementById("nav-cont-side");
-let docker = document.getElementById("docker");
-let line_one = document.getElementById("line-one");
-let line_two = document.getElementById("line-two");
-let line_three = document.getElementById("line-three");
+// let nav_side = document.getElementById("nav-cont-side");
+// let docker = document.getElementById("docker");
+// let line_one = document.getElementById("line-one");
+// let line_two = document.getElementById("line-two");
+// let line_three = document.getElementById("line-three");
 
-if(nav_side && docker && line_two && line_one && line_three) {
-    docker.onclick = function() {
-        if (line_one.style.top == "2px") {
-            alert("Success");
+// if(nav_side && docker && line_two && line_one && line_three) {
+//     docker.onclick = function() {
+//         if (line_one.style.top == "2px") {
+//             alert("Success");
+//         } else {
+//             console.log(line_one.style);
+//         }
+//     }
+// }
+
+docker.onclick = function () {
+    let to_hide = document.getElementsByClassName("to-hide");
+
+    for (const element of to_hide) {
+        if(element.classList.contains("show")) {
+
+            element.classList.add("hide");
+            
+            setTimeout(function () {
+                element.classList.remove("show");
+                element.classList.remove("hide");
+            }, 400)
+
         } else {
-            console.log(line_one.style);
+            element.classList.add("show");
         }
     }
 }
